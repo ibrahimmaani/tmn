0688a42978a59660a129f68b739c52b77ad0d579
ibrahimmaani <ibrahim.maani@gmail.com>
2018-01-10 12:20:24 +0700

    screen detail

--- app/src/main/java/com/example/commerce/ImageAdapter.java
+++ app/src/main/java/com/example/commerce/ImageAdapter.java
@@ -34,9 +34,9 @@ public class ImageAdapter extends BaseAdapter{
         if (convertView == null) {
             // if it's not recycled, initialize some attributes
             imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(180, 185));
+            imageView.setLayoutParams(new GridView.LayoutParams(210, 190));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
+            imageView.setPadding(10, 10, 10, 10);
             imageView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
